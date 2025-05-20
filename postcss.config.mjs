4e51fb4f62cf0ec81ee278708d57247b22677931
gwatsin <gwatsinthong@gmail.com>
2025-05-20 22:28:42 +0530

    setup

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  }
-}
+// filepath: c:\Users\gwats\OneDrive\Desktop\Projects\xora\postcss.config.mjs
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+const config = {
+  plugins: [tailwindcss, autoprefixer],
+};
+
+export default config;
