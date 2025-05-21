96127da76422dcdd3fbf9ef83ea370a2c586a34e
gwatsin <gwatsinthong@gmail.com>
2025-05-21 16:31:23 +0530

    hiatus

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-poppins font-bold underline">
-        Hello World
-      </h1>
-    </div>
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+    </main>
   )
 }
 
