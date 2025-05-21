83c8f867658327b1fb32d08222c3460094dfea1b
gwatsin <gwatsinthong@gmail.com>
2025-05-21 11:50:23 +0530

    added all css utilities

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-poppins font-bold underline">
         Hello World
       </h1>
     </div>
